73e234a1918fca27e0e83af25a7c63862d031cf4
francisco-solis99 <franciscosoliscss@gmail.com>
2022-12-07 13:45:01 -0600

    [Add] the Home page structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,72 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import '../styles/pages/home.css';
 import videoMobile from '../assets/soundwave-middle.mp4';
 import videoDesktop from '../assets/soundwave-full.mp4';
-import '../styles/pages/home.css';
+
+import TopSongs from '../components/TopSongs';
+import { getSongsTops } from '../services/topSongs/topSongs';
+
 
 
 export default function Home() {
+
+  const [topSongs, setTopSongs] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      getSongsTops()
+        .then(data => {
+          setTopSongs(data);
+        })
+        .catch(err => console.log(err))
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }, 200);
+
+  }, []);
+
+
   return (
     <main className="Home__main">
-      <section className="section container Home__CTA">
+      <section className="section Home__CTA">
         <div className="overlay"></div>
         <video className='Home__video' muted autoPlay loop>
           <source src={videoMobile} type="video/mp4" />
           <source src={videoDesktop} type="video/mp4" media="all and (min-width: 768px)" />
         </video>
-        <div className="Home__copy">
-          <h1 className="Home__title">SoundWave</h1>
-          <p className="Home__sub-copy">Curabitur rhoncus ut magna sit amet fringilla. Nullam placerat lacus vel arcu volutpat, sed efficitur justo porta</p>
-          <button className="Home__button">Get Started</button>
+        <div className="container">
+          <div className="Home__copy">
+            <h1 className="Home__title">SoundWave</h1>
+            <p className="Home__sub-copy">Curabitur rhoncus ut magna sit amet fringilla. Nullam placerat lacus vel arcu volutpat, sed efficitur justo porta</p>
+            <button className="Home__button">Get Started</button>
+          </div>
         </div>
       </section>
-      <section className="section container">
-        <h2>Tops</h2>
+      <section className="section Home__tops" data-aos="fade-up">
+        <div className="container">
+          <h2 className="Home__title-section">Tops</h2>
+          {
+            !isLoading ? topSongs.map(top => <TopSongs key={top.id} topData={top} />) : <p>Loading...</p>
+          }
+        </div>
       </section>
-      <section className="section container">
-        <h2>Songs</h2>
+      <section className="section Home__songs">
+        <div className="container">
+          <h2 className="Home__title-section">Songs</h2>
+        </div>
       </section>
-      <section className="section container">
-        <h2>Artists</h2>
+      <section className="section Home__artists">
+        <div className="container">
+          <h2 className="Home__title-section">Artists</h2>
+        </div>
       </section>
-      <section className="section container">
-        <h2>Genres</h2>
+      <section className="section Home__genres">
+        <div className="container">
+          <h2 className="Home__title-section">Genres</h2>
+        </div>
       </section>
     </main>
   );
