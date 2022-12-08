11ad298e28996a75b8d16bc60243dc2eb9a1debb
francisco-solis99 <franciscosoliscss@gmail.com>
2022-12-07 19:43:25 -0600

    [Add] deezer config API

---
+++ src/services/deezer.js
@@ -0,0 +1,15 @@
+export const deezerAPI = {
+  baseURL: 'https://deezerdevs-deezer.p.rapidapi.com/',
+  options: {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'ded9988583mshbdfb1ea421b419bp1cb31fjsn411e9a577f74',
+      'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+    }
+  }
+};
+
+
+
+export default deezerAPI;
+
