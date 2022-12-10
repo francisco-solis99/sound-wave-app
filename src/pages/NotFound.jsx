9d9481a3c9c92548d1c36539a31be2065f94c3f8
marmelendez <lizbethmelendez@live.com>
2022-12-10 17:00:30 -0600

    Update NotFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import '../styles/pages/notfound.css';
+import NotFoundImg from '../assets/notfound.png';
 
 export default function NotFound() {
   return (
-    <h2>NotFound</h2>
+    <div className='NotFound'>
+      <img src={NotFoundImg} alt="Not found icon" className='NotFound__img' />
+
+      <p>Sorry! The page you were looking for could not be found. <br /><br />
+        But please don't stop the music, <a href="" className='NotFound__return'> go back home </a>.</p>
+    </div>
   );
 }
