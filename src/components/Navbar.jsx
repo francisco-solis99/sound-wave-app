a13560633090ccd626dd14a846a41f9cdbabd768
AlmaLSt <alma.loverasotelo@gmail.com>
2022-12-09 20:41:17 -0600

    [Change] icons to MUI components and added responsive design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../styles/components/navbar.css';
 import SearchBar from './SearchBar';
-import UserIcon from '../assets/icons/user.svg';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 
 export default function NavBar(){
     return(
         <div className='navbar'>
             <SearchBar/>
             <button className='user__icon'>
-            <img src={UserIcon} alt='user icon'/>
+            <PersonOutlineOutlinedIcon/>
             </button>   
         </div>
     ); 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import '../styles/components/searchbar.css';
-// import SearchIcon from '@mui/icons-material/Search';
-import searchIcon from '../assets/icons/loupe.svg';
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function SearchBar(){
     return(
         <div className='search__bar'>
             <input className='search__input' type='text' placeholder='SEARCH'/>
             <button className='search__button'>
-            <img src={searchIcon} alt='search icon'/>
+            <SearchIcon/> 
             </button>
         </div>
     );   
