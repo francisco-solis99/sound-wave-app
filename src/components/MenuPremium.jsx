c8008c8e65e677d5cb21a967031215f7c5c44c54
AlmaLSt <alma.loverasotelo@gmail.com>
2022-12-12 13:45:46 -0600

    [Updated] menu responsive design

--- src/components/MenuPremium.jsx
+++ src/components/MenuPremium.jsx
@@ -1,38 +1,51 @@
-import React from "react";
-import "../styles/components/menuPremium.css";
-import Breadcrumbs from "@mui/material/Breadcrumbs";
-import Link from "@mui/material/Link";
-import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
-import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/components/menuPremium.css';
+import MenuIcon from '@mui/icons-material/Menu';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 
 export default function MenuPremium() {
+  const [showBar, setShowBar] = useState(false);
+
   return (
     <div className="menu__container">
-        <h1 className="logo__menu">
-          <Link href="/home">Soundwave</Link>
-        </h1>
-        <div className="side__container">
-          <div className="breadcrumbs">
-            <Breadcrumbs aria-label="breadcrumb">
-              <Link underline="hover" href="/">
-                Songs
-              </Link>
-              <Link underline="hover" href="/">
-                Artists
-              </Link>
-              <Link underline="hover" href="/">
-                Genres
-              </Link>
-            </Breadcrumbs>
-          </div>
-          <button className="user__icon">
-            <PersonOutlineOutlinedIcon />
-          </button>
-          <button className="exit__button">
-            <LogoutOutlinedIcon />
-          </button>
+      <h1 className="logo__menu">
+        <Link to="/home">Soundwave</Link>
+      </h1>
+      <div className="side__container">
+        <button
+          className="nav__toggle"
+          onClick={() => {
+            setShowBar(!showBar);
+          }}
+        >
+          <MenuIcon />
+        </button>
+        <div className="nav__menu" id={showBar ? 'hidden': ''}>
+          <ul className="list__menu">
+            <li>
+              <Link to="/songs">Songs</Link>
+            </li>
+            <li>
+              <Link to="/artists">Artists</Link>
+            </li>
+            <li>
+              <Link to="/genres">Genres</Link>
+            </li>
+          </ul>
         </div>
-  
+        <button className="user__icon">
+          <Link to="/signup">
+            <PersonOutlineOutlinedIcon />
+          </Link>
+        </button>
+        <button className="exit__button">
+          <Link to="/dashboard">
+            <LogoutOutlinedIcon />
+          </Link>
+        </button>
+      </div>
     </div>
   );
 }
