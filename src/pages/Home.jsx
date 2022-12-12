5c15dae7457f9e1289bd443b2f952546b666bf46
marmelendez <lizbethmelendez@live.com>
2022-12-11 20:56:05 -0600

    Update Home Page sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
 import '../styles/pages/home.css';
@@ -6,39 +7,39 @@ import videoMobile from '../assets/soundwave-middle.mp4';
 import videoDesktop from '../assets/soundwave-full.mp4';
 
 import Loader from '../components/Loader';
-import TopSongs from '../components/TopSongs';
-import ArtistSlider from '../components/ArtistSlider';
-import ModalArtist from '../components/ModalArtist';
-import SongsList from '../components/SongsList';
 import Button from '../components/Button';
+import TopSlider from '../components/TopSlider';
+import SongsList from '../components/SongsList';
+import ModalArtist from '../components/ModalArtist';
+import ArtistSlider from '../components/ArtistSlider';
+import GenreSlider from '../components/GenreSlider';
+// //import TopSongs from '../components/TopSongs';
 import { getSongsTops } from '../services/topSongs/topSongs';
 import { getSongsWithSample } from '../services/songs/songs';
 import { getAllArtists } from '../services/artists/artists';
 import { getAllGenres } from '../services/genres/genres';
-import GenreSlider from '../components/GenreSlider';
 
 export default function Home() {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
   const [topSongs, setTopSongs] = useState([]);
   const [songs, setSongs] = useState([]);
+  const [modalArtistData, setModalArtistData] = useState({});
   const [artists, setArtists] = useState([]);
   const [genres, setGenres] = useState([]);
-  const [modalArtistData, setModalArtistData] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
-  const navigate = useNavigate();
+
+  const handleClickStarted = () => {
+    navigate('/dashboard');
+  };
 
   useEffect(() => {
     setIsLoading(true);
     setTimeout(() => {
       getSongsTops()
-        .then(data => {
-          setTopSongs(data);
-        })
+        .then(data => setTopSongs(data))
         .catch(err => console.log(err))
-        .finally(() => {
-          setIsLoading(false);
-        });
+        .finally(() => setIsLoading(false));
     }, 100);
-
   }, []);
 
   useEffect(() => {
@@ -50,113 +51,102 @@ export default function Home() {
           setSongs(songListUI);
         })
         .catch(err => console.log(err))
-        .finally(() => {
-          setIsLoading(false);
-        });
+        .finally(() => setIsLoading(false));
     }, 100);
-  }, []);
-
-  useEffect(() => {
-    setIsLoading(true);
-    setTimeout(() => {
-      getSongsTops()
-        .then(data => {
-          setTopSongs(data);
-        })
-        .catch(err => console.log(err))
-        .finally(() => {
-          setIsLoading(false);
-        });
-    }, 100);
-
   }, []);
 
   useEffect(() => {
     setIsLoading(true);
     setTimeout(() => {
       getAllArtists()
-        .then(data => {
-          setArtists(data);
-        })
+        .then(data => setArtists(data))
         .catch(err => console.log(err))
-        .finally(() => {
-          setIsLoading(false);
-        });
+        .finally(() => setIsLoading(false));
     }, 100);
   }, []);
 
-
-  const handleClickStarted = () => {
-    navigate('/dashboard');
-  };
   useEffect(() => {
     setIsLoading(true);
     setTimeout(() => {
       getAllGenres()
-        .then(data => {
-          setGenres(data);
-        })
+        .then(data => setGenres(data))
         .catch(err => console.log(err))
-        .finally(() => {
-          setIsLoading(false);
-        });
-    }, 200);
+        .finally(() => setIsLoading(false));
+    }, 100);
   }, []);
 
   return (
     <main className="Home__main">
-      <section className="section Home__CTA">
-        <div className="overlay"></div>
-        <video className='Home__video' muted autoPlay loop>
-          <source src={videoMobile} type="video/mp4" />
-          <source src={videoDesktop} type="video/mp4" media="all and (min-width: 768px)" />
-        </video>
-        <div className="container">
-          <div className="Home__copy">
-            <h1 className="Home__title">SoundWave</h1>
-            <p className="Home__sub-copy">Curabitur rhoncus ut magna sit amet fringilla. Nullam placerat lacus vel arcu volutpat, sed efficitur justo porta</p>
-            <Button type="secundary" text="Get started" onClick={handleClickStarted}>
-              Get started
-            </Button>
+      <div className="Home__section-container">
+
+        {/* ----------------- START OF LANDING SECTION ----------------- */}
+        <section className="section Home__CTA">
+          <div className="overlay"></div>
+          <video className='Home__video' muted autoPlay loop>
+            <source src={videoMobile} type="video/mp4" />
+            <source src={videoDesktop} type="video/mp4" media="all and (min-width: 768px)" />
+          </video>
+
+          <div className="container Home__container">
+            <div className="Home__copy">
+              <h1 className="Home__title">SoundWave</h1>
+              <p className="Home__sub-copy">This website will provide you with many songs, artists and genres included in the top lists over the years. Enjoy!</p>
+              <Button type="secundary" text="Get started" onClick={handleClickStarted}>
+                Get started
+              </Button>
+            </div>
           </div>
-        </div>
-      </section>
-      <section className="section Home__tops">
-        <div className="container">
-          <h2 className="Home__title-section">Tops</h2>
-          {
-            !isLoading ? topSongs.map(top => <TopSongs key={top.id} topData={top} />) : <Loader />
-          }
-        </div>
-      </section>
-      <section className="section Home__songs">
-        <div className="container">
-          <h2 className="Home__title-section">Songs</h2>
-          <div className="Home__songs-list">
+        </section>
+        {/* ----------------- END OF LANDING SECTION ----------------- */}
+
+        <section className="section Home__tops">
+          <div className="container Home__container">
+            <h2 className="Home__title-section">Tops</h2>
             {
-              !isLoading ? <SongsList songs={songs} /> : <Loader />
+              !isLoading ? <TopSlider topData={topSongs} /> : <Loader />
+              // topSongs.map(top => <TopSongs key={top.id} topData={top} />)
             }
           </div>
-        </div>
-      </section>
-      <section className="section Home__artists">
-        <div className="container">
-          <h2 className="Home__title-section">Artists</h2>
-          <ModalArtist artistData={modalArtistData} />
+        </section>
 
-          {
-            !isLoading ? <ArtistSlider artists={artists} setModalArtistData={setModalArtistData} /> : <Loader />
-          }
-        </div>
-      </section>
-      <section className="section Home__genres">
-        <div className="container">
-          <h2 className="Home__title-section">Genres</h2>
-          {
-            !isLoading ? <GenreSlider genres={genres} /> : <Loader />
-          }
-        </div>
-      </section>
+        <section className="section Home__songs">
+          <div className="container Home__container">
+            <h2 className="Home__title-section">Songs</h2>
+            <div className="Home__songs-list">
+              {
+                !isLoading ? <SongsList songs={songs} /> : <Loader />
+              }
+            </div>
+            <Link to="/songs" className='Home__more-link'>Ver mas</Link>
+          </div>
+        </section>
+
+        {/* ----------------- START OF ARTISTS SECTION ----------------- */}
+        <section className="section Home__artists">
+          <div className="container Home__container">
+            <h2 className="Home__title-section">Artists</h2>
+            <ModalArtist artistData={modalArtistData} />
+            {
+              !isLoading ? <ArtistSlider artists={artists} setModalArtistData={setModalArtistData} /> : <Loader />
+            }
+            <Link to="/artists" className='Home__more-link'>Ver mas</Link>
+          </div>
+        </section>
+        {/* ----------------- END OF ARTISTS SECTION ----------------- */}
+
+        {/* ----------------- START OF GENRES SECTION ----------------- */}
+        <section className="section Home__genres">
+          <div className="container Home__container">
+            <h2 className="Home__title-section">Genres</h2>
+            {
+              !isLoading ? <GenreSlider genres={genres} /> : <Loader />
+            }
+            <Link to="/genres" className='Home__more-link'>Ver mas</Link>
+          </div>
+        </section>
+        {/* ----------------- END OF GENRES SECTION ----------------- */}
+
+      </div>
     </main>
   );
 }
