0c1118c1a9aad1c4d667774b8451b10973cff1d1
marmelendez <lizbethmelendez@live.com>
2022-12-13 12:27:03 -0600

    Add create top method

--- src/services/tops/tops.js
+++ src/services/tops/tops.js
@@ -10,7 +10,6 @@ export const getTops = async () => {
   }
 };
 
-
 export const getTopsByUser = async (userId) => {
   try {
     const tops = await fetch(`${configAPI.BASE_URL}/tops/user/${userId}`);
@@ -18,5 +17,32 @@ export const getTopsByUser = async (userId) => {
   } catch (err) {
     console.log(err);
     return [];
+  }
+};
+
+export const createTop = async (e, name, description, userid) => {
+  e.preventDefault();
+  try {
+    let res = await fetch(`${configAPI.BASE_URL}/tops`, {
+      method: 'POST',
+      body: JSON.stringify({
+        name: name,
+        description: description,
+        userID: userid
+      }),
+    });
+    await res.json();
+
+    console.log(res);
+
+    // if (res.status === 200) {
+    //   setName("");
+    //   setEmail("");
+    //   setMessage("User created successfully");
+    // } else {
+    //   setMessage("Some error occured");
+    // }
+  } catch (err) {
+    console.log(err);
   }
 };
