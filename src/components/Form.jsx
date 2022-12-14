8675821d38ea74e030d22dde5c5c2f54784eb329
francisco-solis99 <franciscosoliscss@gmail.com>
2022-12-13 21:56:29 -0600

    [Refactor] Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import '../styles/components/form.css';
 
-export default function Form({ children }) {
+export default function Form({ children, callbackSubmit }) {
+
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    callbackSubmit();
+  };
+
   return (
-    <form className='Form'>
+    <form className='Form' onSubmit={handlerSubmit}>
       {children}
     </form>
   );
