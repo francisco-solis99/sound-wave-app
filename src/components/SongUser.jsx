463df2c80245239f6d3c576d64e81f1815c99664
marmelendez <lizbethmelendez@live.com>
2022-12-15 09:42:56 -0600

    Remove artist name to prevent bug

--- src/components/SongUser.jsx
+++ src/components/SongUser.jsx
@@ -3,14 +3,15 @@ import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
 import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
 
 export default function SongUser(props) {
+  console.log(props.songData);
   return (
     <div className='dashboard__item SongUser'>
       <div className='item__icon'>
         <LibraryMusicIcon />
       </div>
       <div className='item__info'>
-        <p className='Song__name'>{props.songData.name} ({props.songData.year})</p>
-        <p className='Song__name'>{props.songData.artist.name}  <AddCircleOutlineRoundedIcon data-bs-toggle='modal' data-bs-target='#modalAddToTop' onClick={ () => props.setSelectedSong(props.songData.id)}/> </p>
+        <p className='Song__name'>{props.songData.name}</p>
+        <p className='Song__name'>{props.songData.year}<AddCircleOutlineRoundedIcon data-bs-toggle='modal' data-bs-target='#modalAddToTop' onClick={ () => props.setSelectedSong(props.songData.id)}/> </p>
       </div>
     </div>
   );
